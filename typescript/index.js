f907b9a3e0fc1307286998b56ca378b9e226387b
Ajaybkn <ajaykhangarotsingh@gmail.com>
2024-09-09 23:27:37 +0530

    enums

--- typescript/index.js
+++ typescript/index.js
@@ -1,12 +1,6 @@
+"use strict";
 // function add(a:number,b:number):number{
 //     return a+b
 // }
-// console.log(add(2,3))
-//  const multiply=(a:number,b:number):number=>{
-//     return a*b
-//  }
-//  console.log(multiply(2,3))
-var areaCircle = function (base, height) {
-    return 0.5 * base * height;
-};
-console.log(areaCircle(16, 25));
+const person = ['ajay', 33, true];
+console.log(person[0]);
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,21 +1,53 @@
-function add(a:number,b:number):number{
-    return a+b
-}
+// function add(a:number,b:number):number{
+//     return a+b
+// }
 
 
-console.log(add(2,3))
- const multiply=(a:number,b:number):number=>{
+// console.log(add(2,3))
+//  const multiply=(a:number,b:number):number=>{
 
-    return a*b
+//     return a*b
 
- }
+//  }
 
- console.log(multiply(2,3))
-
-
- const areaCircle=(base:number,height:number):number=>{
-    return 0.5*base*height
- }
+//  console.log(multiply(2,3))
 
 
- console.log(areaCircle(16,25))
+//  const areaCircle=(base:number,height:number):number=>{
+//     return 0.5*base*height
+//  }
+
+
+//  console.log(areaCircle(16,25))
+
+
+// type Student={
+//    name:string,
+//    age:number,
+//    greet:() => void
+// }
+
+// let student:Student ={
+//    name:'ajay',
+//    age:30,
+//    greet():void{
+//        console.log("fhiekds")
+//    }
+// }
+
+// student.greet()
+//enums
+// enum Role{
+//    admin,manager,super_admin
+// }
+
+// console.log(Role[2])
+
+//tuples-->>
+
+type info=readonly [string,number,boolean]
+
+const person:info=['ajay',33,true]
+
+
+console.log(person)
